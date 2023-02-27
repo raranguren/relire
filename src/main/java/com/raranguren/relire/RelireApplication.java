59fddb40d38fee4fd272e1b859440fad5b701c45
raranguren <ricaragas@hotmail.com>
2023-02-27 20:26:53 +0100

    project created

---
+++ src/main/java/com/raranguren/relire/RelireApplication.java
@@ -0,0 +1,13 @@
+package com.raranguren.relire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raranguren/relire/RelireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raranguren.relire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
